bceedd621bdc224b2d6e797f865f4f41933d8b59
sorhawell <sorhawell@gmail.com>
2023-06-05 12:28:23 +0200

    two_3_arg_fns + use pr555

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -7,10 +7,22 @@ fn hello_world() -> &'static str {
     "Hello world!"
 }
 
+#[extendr]
+fn three_args(a: i32, b: String, c: f64) {
+    rprintln!("a:{}, b:{}, c:{}", a, b, c);
+}
+
+#[extendr(use_try_from = true)]
+fn three_args_use_try_from(a: i32, b: String, c: f64) {
+    rprintln!("a:{}, b:{}, c:{}", a, b, c);
+}
+
 // Macro to generate exports.
 // This ensures exported functions are registered with R.
 // See corresponding C code in `entrypoint.c`.
 extendr_module! {
     mod helloextendr;
     fn hello_world;
+    fn three_args;
+    fn three_args_use_try_from;
 }
